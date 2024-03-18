229e99d51797b88543c2aab02c6aacd83186108e
akbar-farajov <akbarfarajov74@gmail.com>
2024-03-18 23:01:12 +0400

    adding burger menu

--- src/Components/Layout/Header/Header.jsx
+++ src/Components/Layout/Header/Header.jsx
@@ -2,22 +2,24 @@ import Navbar from './Navbar';
 import Actions from './Actions';
 import HeaderBottom from './HeaderBottom';
 
-
 import './Header.css';
 import logo from '../../../../public/Header-images/logo.png';
-import menu from "../../../../public/Header-images/burger-menu.png"
-
-
-
+import menu from '../../../../public/Header-images/burger-menu.png';
+import Menu from '../UI/burger-menu/Menu';
 
 function Header() {
+    function handleClick(){
+        
+    }
     return (
         <header className="header">
             <div className="header__top">
                 <div className="container">
                     <div className="top__items">
                         <div className="menu">
-                            <img src={menu} alt="" />
+                     
+                                <img src={menu} alt=""  onClick={handleClick}/>
+                            
                         </div>
                         <a className="logo">
                             <img src={logo} alt="" />
@@ -27,6 +29,8 @@ function Header() {
                     </div>
                 </div>
             </div>
+            <Menu />
+
             <HeaderBottom />
         </header>
     );
