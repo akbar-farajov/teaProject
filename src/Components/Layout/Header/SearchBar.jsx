93d4c0a471c5e2b1ab15a2f5d66be1f9cbb40830
akbar-farajov <akbarfarajov74@gmail.com>
2024-04-02 17:28:53 +0400

    adding searchbar

--- src/Components/Layout/Header/SearchBar.jsx
+++ src/Components/Layout/Header/SearchBar.jsx
@@ -2,7 +2,7 @@
 
 export default function SearchBar() {
     return (
-        <div className="absolute z-10 top-8  right-0">
+        <div className="absolute z-10 top-8   right-0 ">
             <input
                 type="text"
                 className="border-2 border-solid  border-neutral-400 rounded-xl px-3 py-1"
